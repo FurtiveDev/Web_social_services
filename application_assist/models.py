b6c3361b2cac05ae848d924acaf57a848724f3da
Dev <etibar.19732@gmail.com>
2023-10-21 21:48:43 +0300

    Added models

--- application_assist/models.py
+++ application_assist/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class RequestServices(models.Model):
+    id_request_services = models.AutoField(primary_key=True)
+    id_request = models.ForeignKey('Requests', models.DO_NOTHING, db_column='id_request', blank=True, null=True)
+    id_service = models.ForeignKey('Services', models.DO_NOTHING, db_column='id_service', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'request_services'
+
+
+class Requests(models.Model):
+    id_request = models.AutoField(primary_key=True)
+    status = models.CharField(max_length=20, blank=True, null=True)
+    creation_date = models.DateField(blank=True, null=True)
+    formation_date = models.DateField(blank=True, null=True)
+    completion_date = models.DateField(blank=True, null=True)
+    creator = models.IntegerField(blank=True, null=True)
+    moderator = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'requests'
+
+
+class Services(models.Model):
+    id_service = models.AutoField(primary_key=True)
+    service_name = models.CharField(max_length=255, blank=True, null=True)
+    status = models.CharField(max_length=10, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    image = models.BinaryField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'services'
+
+
+class Users(models.Model):
+    id_user = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+    email = models.CharField(max_length=100, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'users'
--- social_assistance/settings.py
+++ social_assistance/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'social_assistance.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Web_service_data',
+        'USER': 'postgres',
+        'PASSWORD': 'gaga12345',
+        'HOST': 'localhost',
+        'PORT': 5432, # Стандартный порт PostgreSQL
     }
 }
 
